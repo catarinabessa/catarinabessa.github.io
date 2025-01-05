7ffb3b4aeb80637ecd259f5d62566aa0351dfd0a
Catarina <catarina.marques.bessa@gmail.com>
2025-01-05 17:06:03 +0000

    semi semi final

--- checking_commission_url.js
+++ checking_commission_url.js
@@ -26,9 +26,10 @@ async function checkFormStatus() {
     if (response) {
         document.getElementById('commission-status').innerHTML =
             '<a class="headline" href="https://docs.google.com/forms/d/e/1FAIpQLScJ90IWf4vAImuPQhIBu35K3_3otwpHyJaRMenO5RfhwalPug/viewform" target="_blank">commissions are open!!</a>';
-    } else {
-        document.getElementById('commission-status').textContent = 'commissions are closed';
-    }
+        } else {
+            document.getElementById('commission-status').innerHTML = '<div class="headline">commissions are closed</div>';
+        }
+        
 }
 
 checkFormStatus();
